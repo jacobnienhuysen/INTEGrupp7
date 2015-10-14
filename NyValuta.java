fd5e897f908f2aa762179b8785fbf563e5c4be8a
Paulina Palmé <paulina.palme@bredband.net>
2015-10-14 13:09:53 +0200

    Valuta-klass
    Valuta-klass

---
+++ NyValuta.java
@@ -0,0 +1,23 @@
+package kvittoTDD;
+
+import java.math.BigDecimal;
+
+public class NyValuta {
+
+	String valutaNamn;
+	double valutakurs;
+	
+	public NyValuta(String namn, double kurs) {
+	valutaNamn = namn;
+	valutakurs = kurs;
+	}
+	public String getValutaNamn(){
+		return valutaNamn;
+	}
+	public double getValutaKurs() {
+		return valutakurs;
+	}
+
+	
+
+}
---
+++ ValutaTest.java
@@ -0,0 +1,42 @@
+package kvittoTDD;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+
+public class ValutaTest {
+
+	@Test
+	public void newValutaOmvandling() {
+	NyValuta valuta = new NyValuta("sek",1);
+	
+	}
+	
+	@Test
+	public void testaNamn(){
+		NyValuta valuta = new NyValuta("sek",1);
+		assertEquals("sek",valuta.getValutaNamn());
+		
+	}
+
+	@Test
+	public void testaNamnEuro(){
+	NyValuta valuta = new NyValuta("euro",9.27);
+	assertEquals("euro",valuta.getValutaNamn());
+	}
+	
+	@Test
+	public void testaNamnDollar(){
+	NyValuta valuta = new NyValuta("dollar",8.13);
+	assertEquals("dollar",valuta.getValutaNamn());
+	
+	}
+	@Test
+	public void testKurs(){
+		NyValuta valuta = new NyValuta("dollar",8.13);
+		assertEquals(8.13,valuta.getValutaKurs(), 0);
+	}
+
+}
