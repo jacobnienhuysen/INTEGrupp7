ebeb2f50c2ff90fe6f3518f5354d45a04dc16689
jacobnienhuysen <jacob.nienhuysen@gmail.com>
2015-10-15 14:42:07 +0200

    Updaterad varuklass
    Implementerat hg, kg och streckkod i varuklassen.

--- Klassfiler/Testfiler/VaraTest.java
+++ Klassfiler/Testfiler/VaraTest.java
@@ -9,72 +9,101 @@ public class VaraTest {
 	
 	@Test
 	public void createVaraTest() {
-		Vara v = new Vara("Toalettpapper", 67.5, "g");
+		Vara v = new Vara("Toalettpapper", 67.5, "g", "0987654321234");
 	}
 	
 	@Test
 	public void getVarasNamn(){
-		Vara v = new Vara("Lättmjölk", 8.5, "st");
+		Vara v = new Vara("Lättmjölk", 8.5, "st", "0987654321234");
 		assertEquals("Lättmjölk", v.getNamn());
 		
-		Vara g = new Vara("Grillkol", 39.90, "st");
+		Vara g = new Vara("Grillkol", 39.90, "st", "0987654321234");
 		assertEquals("Grillkol", g.getNamn());
 	}
 	
 	@Test
 	public void getVarasJfrPris(){
-		Vara v = new Vara("Lättmjölk", 8.50, "st");
+		Vara v = new Vara("Lättmjölk", 8.50, "st", "0987654321234");
 		assertEquals(8.50, v.getJfrPris(), 0.001);
 		
-		Vara f = new Vara("Hundmat", 90, "st");
+		Vara f = new Vara("Hundmat", 90, "st", "0987654321234");
 		assertEquals(90, f.getJfrPris(), 0.001);
 	}
 	
 	@Test
 	public void getVarasEnhet(){
-		Vara v = new Vara("Lättmjölk", 8.50, "st");
+		Vara v = new Vara("Lättmjölk", 8.50, "st", "0987654321234");
 		assertEquals("st", v.getEnhet());
 		
-		Vara g = new Vara("Äpplen", 0.01995, "g");
+		Vara g = new Vara("Saffran", 40, "g", "0987654321234");
 		assertEquals("g", g.getEnhet());
+		
+		Vara h = new Vara("Lösgodis", 25, "hg", "0987654321234");
+		assertEquals("hg", h.getEnhet());
+		
+		Vara k = new Vara("Äpplen", 19.45, "kg", "0987654321234");
+		assertEquals("kg", k.getEnhet());
+		
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void enterFelaktigEnhet(){
-		Vara v = new Vara("Gurka", 19.90, "m");
+		Vara v = new Vara("Gurka", 19.90, "m", "0987654321234");
 		
-		Vara g = new Vara("Mineralvatten", 29.50, "l");
+		Vara g = new Vara("Mineralvatten", 29.50, "l", "0987654321234");
 		
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void enterFelaktigtPris(){
-		Vara v = new Vara("Senap", -0.01, "st");
+		Vara v = new Vara("Senap", -0.01, "st", "0987654321234");
 		
-		Vara f = new Vara("Senap", Double.MIN_VALUE, "st");
+		Vara f = new Vara("Senap", Double.MIN_VALUE, "st", "0987654321234");
 		
-		Vara g = new Vara("Gräslök", Double.MAX_VALUE , "g");
+		Vara g = new Vara("Gräslök", Double.MAX_VALUE , "g", "0987654321234");
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void ingetVarunamn(){
-		Vara v = new Vara("", 25, "st");
+		Vara v = new Vara("", 25, "st", "0987654321234");
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void ogiltigtNamn(){
-		Vara g = new Vara(" ", 16, "st");
+		Vara g = new Vara(" ", 16, "st", "0987654321234");
 		
 	}
 	
 	@Test
+	public void testaGetStreckkod(){
+		Vara g = new Vara("Sand", 1, "st", "0987654321234");
+		assertEquals("0987654321234", g.getStreckkod());
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testaStreckodTecken(){
+		Vara g = new Vara("Grus", 3, "kg", "8980dsdsd");
+		
+		Vara f = new Vara("Sten", 45, "st", "---  ");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testaStreckkodLängd(){
+		Vara g = new Vara("Pengar", 6, "hg", "858585858599");
+		Vara d = new Vara("Pengar", 6, "kg", "00000987654321");
+	}
+	
+	@Test
 	public void toStringTest(){
-		Vara g = new Vara("Äpplen", 0.0299, "g");
+		Vara g = new Vara("Äpplen", 29.90, "kg", "0987654321234");
 		assertEquals("Äpplen, 29,90/kg", g.toString());
 		
-		Vara s = new Vara("Grillkol", 39.90, "st");
+		Vara s = new Vara("Grillkol", 39.90, "st", "0987654321234");
 		assertEquals("Grillkol, 39,90/st", s.toString());
 	}
+	
+	
+	
 	
 
 }
--- Klassfiler/Vara.java
+++ Klassfiler/Vara.java
@@ -6,21 +6,32 @@ public class Vara {
 	private String namn;
 	private double jfrPris;
 	private String enhet;
+	private String streckkod;
 	DecimalFormat formatter = new DecimalFormat("#0.00");  
 	
-	public Vara(String n, double p, String e){
+	public Vara(String n, double p, String e, String sk){
 		if(n.length()<1 || !n.matches(".*[a-zA-Z0-9åäöÅÄÖ]+.*"))
 			throw new IllegalArgumentException("Ogiltigt namn på vara");
 
-		if(!e.equalsIgnoreCase("st") && !e.equalsIgnoreCase("g"))
+		if(!e.equalsIgnoreCase("st") && !e.equalsIgnoreCase("g") && !e.equalsIgnoreCase("hg") && !e.equalsIgnoreCase("kg"))
 			throw new IllegalArgumentException(e + " är inte en giltig enhet!");
 		
 		if(p < 0)
 			throw new IllegalArgumentException(p + " är inte ett giltigt pris!");
 		
+		
+		for (int i = 0; i < sk.length(); i++) {
+			if (!Character.isDigit(sk.charAt(i)))
+				throw new IllegalArgumentException("Streckkoden får enbart innehålla siffror");
+		}
+		
+		if(sk.length()!=13)
+			throw new IllegalArgumentException("Ogiltig längd på streckkoden.");
+		
 		namn = n;
 		jfrPris = p;
 		enhet = e.toLowerCase();
+		streckkod = sk;
 	}
 	
 	public String getNamn(){
@@ -35,10 +46,18 @@ public class Vara {
 		return enhet;
 	}
 	
+	public String getStreckkod(){
+		return streckkod;
+	}
+	
 	public String toString(){
 		if(enhet.equalsIgnoreCase("g")){
 			return namn + ", " + formatter.format(jfrPris*1000) + "/kg";
 		}
+		else if(enhet.equalsIgnoreCase("hg"))
+			return namn + ", " + formatter.format(jfrPris*10) + "/kg";
+		else if(enhet.equalsIgnoreCase("kg"))
+			return namn + ", " + formatter.format(jfrPris) + "/kg";
 		else
 			return namn + ", " + formatter.format(jfrPris) + "/st"; 
 	}
